468abfe17dfba0e727047ddc01f900d45c89c760
ebusy9 <g404ebus@gmail.com>
2023-10-12 23:30:09 +0200

    ajout css

--- assets/app.js
+++ assets/app.js
@@ -1,30 +1,33 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-
-
 const submitBtn = document.querySelector('#sub-btn')
 const token = document.querySelector('#token')
 const chatInput = document.querySelector('#content')
 const messagesDiv = document.querySelector('#messages')
 const form = document.querySelector('#message-form')
+const textarea = document.querySelector('.autosize');
 
 
 submitBtn.addEventListener('click', (event) => {
     event.preventDefault()
     let formData = new FormData(form)
+    chatInput.value = ''
     fetch("api/gpt", {
         method: "POST",
         body: formData
     })
         .then((response) => {
-            console.log(response.json())
-        })
-        .then((data) => {
-            console.log(data)
+            response.json().then((response) => {
+                token.value = response.token
+                messagesDiv.innerHTML = ''
+                response.messages.forEach(message => {
+                    messagesDiv.innerHTML +=
+                        `<div id="${message['role']}">
+                    <div id="date">${message['createdAt']}</div>
+                    <div id="message">${message['content']}</div>
+                    </div>`
+                })
+                messagesDiv.innerHTML += `<div id="bottom-padding"></div>`
+                messagesDiv.scrollTo({top: messagesDiv.scrollHeight})
+            })
         })
 })
 
@@ -43,6 +46,21 @@ getInitialChatContent().then((data) => {
         <div id="date">${message['createdAt']}</div>
         <div id="message">${message['content']}</div>
         </div>`
-    });
+    })
+    messagesDiv.innerHTML += `<div id="bottom-padding"></div>`
+    messagesDiv.scrollTo({top: messagesDiv.scrollHeight})
 })
 
+
+textarea.setAttribute('rows', 1);
+
+resize(textarea);
+
+textarea.addEventListener('input', function () {
+    resize(this);
+});
+
+function resize(textarea) {
+    textarea.style.height = 'auto';
+    textarea.style.height = textarea.scrollHeight + 'px';
+}
