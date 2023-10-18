12d746abaeb4dbf7f9665e20161cfe8bc839344d
ebusy9 <g404ebus@gmail.com>
2023-10-19 01:10:44 +0200

    ajout gestion des erreurs

--- assets/app.js
+++ assets/app.js
@@ -20,7 +20,8 @@ const temperatureInput = document.querySelector('#temperature-input')
 const temperatureInputPlus = document.querySelector('#temperature-input-plus')
 const temperatureInputMinus = document.querySelector('#temperature-input-minus')
 const loadingGifPath = '/assets/images/loading.gif'
-const openAiSvg = '<svg class="openai-icon" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>OpenAI icon</title><path d="M22.2819 9.8211a5.9847 5.9847 0 0 0-.5157-4.9108 6.0462 6.0462 0 0 0-6.5098-2.9A6.0651 6.0651 0 0 0 4.9807 4.1818a5.9847 5.9847 0 0 0-3.9977 2.9 6.0462 6.0462 0 0 0 .7427 7.0966 5.98 5.98 0 0 0 .511 4.9107 6.051 6.051 0 0 0 6.5146 2.9001A5.9847 5.9847 0 0 0 13.2599 24a6.0557 6.0557 0 0 0 5.7718-4.2058 5.9894 5.9894 0 0 0 3.9977-2.9001 6.0557 6.0557 0 0 0-.7475-7.0729zm-9.022 12.6081a4.4755 4.4755 0 0 1-2.8764-1.0408l.1419-.0804 4.7783-2.7582a.7948.7948 0 0 0 .3927-.6813v-6.7369l2.02 1.1686a.071.071 0 0 1 .038.052v5.5826a4.504 4.504 0 0 1-4.4945 4.4944zm-9.6607-4.1254a4.4708 4.4708 0 0 1-.5346-3.0137l.142.0852 4.783 2.7582a.7712.7712 0 0 0 .7806 0l5.8428-3.3685v2.3324a.0804.0804 0 0 1-.0332.0615L9.74 19.9502a4.4992 4.4992 0 0 1-6.1408-1.6464zM2.3408 7.8956a4.485 4.485 0 0 1 2.3655-1.9728V11.6a.7664.7664 0 0 0 .3879.6765l5.8144 3.3543-2.0201 1.1685a.0757.0757 0 0 1-.071 0l-4.8303-2.7865A4.504 4.504 0 0 1 2.3408 7.872zm16.5963 3.8558L13.1038 8.364 15.1192 7.2a.0757.0757 0 0 1 .071 0l4.8303 2.7913a4.4944 4.4944 0 0 1-.6765 8.1042v-5.6772a.79.79 0 0 0-.407-.667zm2.0107-3.0231l-.142-.0852-4.7735-2.7818a.7759.7759 0 0 0-.7854 0L9.409 9.2297V6.8974a.0662.0662 0 0 1 .0284-.0615l4.8303-2.7866a4.4992 4.4992 0 0 1 6.6802 4.66zM8.3065 12.863l-2.02-1.1638a.0804.0804 0 0 1-.038-.0567V6.0742a4.4992 4.4992 0 0 1 7.3757-3.4537l-.142.0805L8.704 5.459a.7948.7948 0 0 0-.3927.6813zm1.0976-2.3654l2.602-1.4998 2.6069 1.4998v2.9994l-2.5974 1.4997-2.6067-1.4997Z"></path></g></svg>'
+const openAiSvg = '<svg class="date-icon" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>OpenAI icon</title><path d="M22.2819 9.8211a5.9847 5.9847 0 0 0-.5157-4.9108 6.0462 6.0462 0 0 0-6.5098-2.9A6.0651 6.0651 0 0 0 4.9807 4.1818a5.9847 5.9847 0 0 0-3.9977 2.9 6.0462 6.0462 0 0 0 .7427 7.0966 5.98 5.98 0 0 0 .511 4.9107 6.051 6.051 0 0 0 6.5146 2.9001A5.9847 5.9847 0 0 0 13.2599 24a6.0557 6.0557 0 0 0 5.7718-4.2058 5.9894 5.9894 0 0 0 3.9977-2.9001 6.0557 6.0557 0 0 0-.7475-7.0729zm-9.022 12.6081a4.4755 4.4755 0 0 1-2.8764-1.0408l.1419-.0804 4.7783-2.7582a.7948.7948 0 0 0 .3927-.6813v-6.7369l2.02 1.1686a.071.071 0 0 1 .038.052v5.5826a4.504 4.504 0 0 1-4.4945 4.4944zm-9.6607-4.1254a4.4708 4.4708 0 0 1-.5346-3.0137l.142.0852 4.783 2.7582a.7712.7712 0 0 0 .7806 0l5.8428-3.3685v2.3324a.0804.0804 0 0 1-.0332.0615L9.74 19.9502a4.4992 4.4992 0 0 1-6.1408-1.6464zM2.3408 7.8956a4.485 4.485 0 0 1 2.3655-1.9728V11.6a.7664.7664 0 0 0 .3879.6765l5.8144 3.3543-2.0201 1.1685a.0757.0757 0 0 1-.071 0l-4.8303-2.7865A4.504 4.504 0 0 1 2.3408 7.872zm16.5963 3.8558L13.1038 8.364 15.1192 7.2a.0757.0757 0 0 1 .071 0l4.8303 2.7913a4.4944 4.4944 0 0 1-.6765 8.1042v-5.6772a.79.79 0 0 0-.407-.667zm2.0107-3.0231l-.142-.0852-4.7735-2.7818a.7759.7759 0 0 0-.7854 0L9.409 9.2297V6.8974a.0662.0662 0 0 1 .0284-.0615l4.8303-2.7866a4.4992 4.4992 0 0 1 6.6802 4.66zM8.3065 12.863l-2.02-1.1638a.0804.0804 0 0 1-.038-.0567V6.0742a4.4992 4.4992 0 0 1 7.3757-3.4537l-.142.0805L8.704 5.459a.7948.7948 0 0 0-.3927.6813zm1.0976-2.3654l2.602-1.4998 2.6069 1.4998v2.9994l-2.5974 1.4997-2.6067-1.4997Z"></path></g></svg>'
+const warningIcon = '<svg class="date-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>'
 let displayedMessages = []
 let isChatInputDisabled = false
 let modelSelectSet = false
@@ -72,12 +73,10 @@ contextLimitInputMinus.addEventListener('click', (e) => {
 })
 
 settingsBtn.addEventListener('click', (e) => {
-    e.preventDefault()
     settingsModal.style.display = 'flex'
 })
 
 messageInput.addEventListener('keypress', function (e) {
-
     if (e.key === 'Enter' && !e.shiftKey) {
         submitFormUpdateMessages(e)
     }
@@ -100,21 +99,24 @@ showHintBtn.addEventListener('click', (e) => {
 
 async function updateMessagesAndForm() {
     try {
-        fetch('api/gpt', {
+        const response = await fetch('api/gpt', {
             method: 'POST'
         })
-            .then((response) => {
-                if (isChatInputDisabled) { return }
-                response.json()
-                    .then((response) => {
-                        if (isChatInputDisabled) { return }
-                        insertMessagesAndConfigureForm(response)
-                    })
-            })
-    } catch (error) {
-        console.log('Failed to load messages ' + error)
-    }
 
+        if (isChatInputDisabled) {
+            return
+        }
+
+        const responseData = await response.json();
+
+        if (isChatInputDisabled) {
+            return
+        }
+
+        insertMessagesAndConfigureForm(responseData);
+    } catch (error) {
+        console.log('Failed to update messages: ' + error)
+    }
 }
 
 function insertMessagesAndConfigureForm(response) {
@@ -208,46 +210,31 @@ async function submitFormUpdateMessages(event) {
     event.preventDefault()
     const formData = new FormData(form)
     const sentMessageContent = formData.get('message[content]')
-    if (isChatInputDisabled) {
-        chatInputDisabledMsg.style.display = 'block'
-        chatInputDisabledMsg.innerHTML = 'Please wait until the message is loaded...'
-        chatInput.style.border = '1px solid #DD4A48'
-        setTimeout(() => {
-            chatInputDisabledMsg.style.display = null
-            chatInput.style.border = null
-        }, 4500)
+
+    if (!verifyConditionsBeforeStubmit(sentMessageContent)) {
         return
     }
-    if (sentMessageContent.replace(/[\r\n]/gm, '').length === 0) {
-        chatInputDisabledMsg.style.display = 'block'
-        chatInputDisabledMsg.innerHTML = 'Message should not be empty.'
-        chatInput.style.border = '1px solid #DD4A48'
-        setTimeout(() => {
-            chatInputDisabledMsg.style.display = null
-            chatInput.style.border = null
-        }, 4500)
-        return
-    }
+
     clearInterval(updateMessagesAndFormInterval)
     isChatInputDisabled = true
     messagesLoading(sentMessageContent)
+
     try {
-        fetch('api/gpt', {
+        const response = await fetch('api/gpt', {
             method: 'POST',
             body: formData
         })
-            .then((response) => {
-                response.json()
-                    .then((response) => {
-                        messagesLoaded(response)
-                        isChatInputDisabled = false
-                        updateMessagesAndFormInterval = setInterval(updateMessagesAndForm, 6000)
-                    })
-            })
+
+        const responseData = await response.json();
+
+        messagesLoaded(responseData)
+
     } catch (error) {
-        console.log('Cannot get a valid response ' + error)
+        warningMsgFormSubmitFailed()
     }
 
+    isChatInputDisabled = false
+    updateMessagesAndFormInterval = setInterval(updateMessagesAndForm, 6000)
 }
 
 function messagesLoading(messageSentContent) {
@@ -259,16 +246,17 @@ function messagesLoading(messageSentContent) {
                 <div class="message-content">${messageSentContent}</div>
             </div>
             <div class="date">${getFormattedDate()}</div>
+        </div>
+        <div class="assistant-container" id="loading-assistant-container" style="display: none;">
+            <div class="assistant">
+                <div class="message-content" id="loading-message-content"><img src="${loadingGifPath}"></div>
+            </div>
+            <div class="date" id="loading-date"><img src="${loadingGifPath}"></div>
         </div>`)
+    const assistantLoadingContainer = document.querySelector('#loading-assistant-container')
     messagesDiv.scrollTo({ top: messagesDiv.scrollHeight, behavior: 'smooth' })
     setTimeout(() => {
-        bottomPaddingDiv.insertAdjacentHTML("beforebegin",
-            `<div class="assistant-container" id="loading-assistant-container">
-                <div class="assistant">
-                    <div class="message-content" id="loading-message-content"><img src="${loadingGifPath}"></div>
-                </div>
-                <div class="date" id="loading-date"><img src="${loadingGifPath}"></div>
-            </div>`)
+        assistantLoadingContainer.style.display = null
         messagesDiv.scrollTo({ top: messagesDiv.scrollHeight, behavior: 'smooth' })
     }, 400)
 }
@@ -332,6 +320,8 @@ function type(loadingMessageContentDiv, messageContent, bottomPaddingHeight, ind
 }
 
 function getFormattedDate(timestamp = Date.now() / 1000, message = false) {
+
+    const dateToday = new Date();
     const date = new Date(timestamp * 1000)
 
     const formattedDate = date.toLocaleString('en-US', {
@@ -346,14 +336,22 @@ function getFormattedDate(timestamp = Date.now() / 1000, message = false) {
 
     const dateWithoutCommas = formattedDate.split(',').join('')
     let dateArray = dateWithoutCommas.split(' ')
-    let mounth = dateArray[1]
-    dateArray[1] = dateArray[2]
-    dateArray[2] = mounth
-    dateArray[3] = '| ' + dateArray[3]
+
+    if (dateToday.getDay() === date.getDay()
+        && dateToday.getMonth() === date.getMonth()
+        && dateToday.getFullYear() === date.getFullYear()) {
+        dateArray.shift()
+        dateArray.shift()
+        dateArray.shift()
+    } else {
+        let mounth = dateArray[1]
+        dateArray[1] = dateArray[2]
+        dateArray[2] = mounth
+        dateArray[3] = '| ' + dateArray[3]
+    }
 
     if (message && message.role === "assistant") {
         dateArray.unshift(' | ')
-
 
         switch (message.model.name) {
             case 'gpt-4':
@@ -387,4 +385,57 @@ function uuidv4() {
 function resize(messageInput) {
     messageInput.style.height = 'auto'
     messageInput.style.height = messageInput.scrollHeight + 'px'
+}
+
+function verifyConditionsBeforeStubmit(sentMessageContent) {
+    if (isChatInputDisabled) {
+        chatInputDisabledMsg.style.display = 'block'
+        chatInputDisabledMsg.innerHTML = 'Please wait until the message is loaded...'
+        chatInput.style.border = '1px solid #DD4A48'
+        setTimeout(() => {
+            chatInputDisabledMsg.style.display = null
+            chatInput.style.border = null
+        }, 4500)
+        return false
+    }
+
+    if (sentMessageContent.trim() === '') {
+        chatInputDisabledMsg.style.display = 'block'
+        chatInputDisabledMsg.innerHTML = 'The message should not be empty.'
+        chatInput.style.border = '1px solid #DD4A48'
+        setTimeout(() => {
+            chatInputDisabledMsg.style.display = null
+            chatInput.style.border = null
+        }, 4500)
+        return false
+    }
+
+    return true
+}
+
+function warningMsgFormSubmitFailed() {
+    const loadingUserContainer = document.querySelector('#loading-user-container')
+    const loadingAssistantContainer = document.querySelector('#loading-assistant-container')
+    const userDiv = loadingUserContainer.querySelector('.user')
+    const assistantDiv = loadingAssistantContainer.querySelector('.assistant')
+    const assistantContainerMsgContent = loadingAssistantContainer.querySelector('.message-content')
+    const userContainerMsgContent = loadingUserContainer.querySelector('.message-content')
+    const assistantContainerDate = loadingAssistantContainer.querySelector('.date')
+    const userContainerDate = loadingUserContainer.querySelector('.date')
+
+    loadingUserContainer.removeAttribute('id')
+    loadingAssistantContainer.removeAttribute('id')
+
+    userDiv.setAttribute('class', 'user loading-failed')
+    assistantDiv.setAttribute('class', 'assistant loading-failed')
+
+    assistantContainerMsgContent.removeAttribute('id')
+    userContainerMsgContent.removeAttribute('id')
+
+    assistantContainerMsgContent.innerHTML = warningIcon + ' An error occurred while processing your request. Please check your internet connection and try again.'
+
+    assistantContainerDate.setAttribute('class', 'date date-loading-failed')
+    userContainerDate.setAttribute('class', 'date date-loading-failed')
+    userContainerDate.innerHTML = warningIcon + ' ' + userContainerDate.innerHTML
+    assistantContainerDate.innerHTML = warningIcon + ' ' + getFormattedDate()
 }
